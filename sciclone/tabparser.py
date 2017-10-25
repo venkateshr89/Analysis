0a225c74ace2e4d3f4f17a69f86b5b77694b9fef
venks <venkateshr89@gmail.com>
2017-10-25 13:09:31 -0700

    updated tabparser with more comments

--- sciclone/tabparser.py
+++ sciclone/tabparser.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3.5
 
+# this script transforms " ann.all_transcripts.tab" from single sample tumor mutation calling to sciclone accepted VAF.dat format
+
 #usage
-### arg1=vcf,
-### arg2=target transcript file in gene,transcript format ,
-### arg3=tcga file in count,chr,pos
+### arg1=ann.transcript.tab from Single sample pipeline or T/N pipeline
+### arg2= output file
+
+### output file should be in the format with header
+#CHROM   POS     TUM1_REF_READS  TUM1_VAR_READS  TUM1_VAF
+#chr1    10146   23      3       38.0
+#chr1    10146   23      3       38.0
+#chr1    10492   2       2       100.0
+
 
 import sys
 import argparse
@@ -34,14 +42,4 @@ for line in open(tab_file):
                     vaf1=float(vaf)*100
                     out_line=chrom+"\t"+bp+"\t"+total_reads+"\t"+var_reads+"\t"+str(vaf1)+"\n"
                     out.write(out_line)
-                    #inter=chrom+"\t"+bp+"\t"+total_reads+"\t"+var_reads+"\t"+vaf
-                    #inter_list.append(inter)
-# j=0
-# for l in inter_list:
-#     j=j+1
-#     col=l.strip().split("\t")
-#     total=float(col[2])
-#     var=float(col[3])
-#     ref=col[2]-col[3]
-#     print j
-#     print(col[0]+"\t"+col[1]+"\t"+ref+"\t"+col[3]+"\t"+col[4])
+                   
